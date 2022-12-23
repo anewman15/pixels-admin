2ad72d37d54b1b4d0b15c8743da7c192b30496d6
Abdullah Numan <anewman15@hotmail.com>
2022-12-23 21:09:30 +0600

    Define auditLogProvider

---
+++ src/providers/auditLogProvider.ts
@@ -0,0 +1,43 @@
+import { AuditLogProvider } from "@pankod/refine-core";
+import { dataProvider } from "@pankod/refine-supabase";
+import { supabaseClient } from "utility";
+
+export const auditLogProvider: AuditLogProvider = {
+  get: async ({ resource, meta }) => {
+    const { data } = await dataProvider(supabaseClient).getList({
+      resource: "logs",
+      filters: [
+        {
+          field: "resource",
+          operator: "eq",
+          value: resource,
+        },
+        {
+          field: "meta.id",
+          operator: "eq",
+          value: meta?.id,
+        },
+      ],
+      // metaData: {
+      //   select: ""
+      // }
+    });
+
+    return data;
+  },
+  create: params => {
+    return dataProvider(supabaseClient).create({
+      resource: "logs",
+      variables: params,
+    });
+  },
+  update: async ({ id, name }) => {
+    const { data } = await dataProvider(supabaseClient).update({
+      resource: "logs",
+      id,
+      variables: { name },
+    });
+
+    return data;
+  }
+};
